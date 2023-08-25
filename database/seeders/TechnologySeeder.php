63cf6c2c0d4624f5841e7b050ef6dd4a9acae24f
S-Lombardi <silo.lombardi@gmail.com>
2023-08-25 18:13:53 +0200

    Creato nuovo progetto con i file necessari. Creata tabella technologies

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+//importo il Model!!!!!!
+use App\Models\Technology;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['Html', 'Css', 'Javascript', 'Php', 'Laravel', 'VueJs', 'JQuery', 'Sass','Symfony'];
+
+        foreach($technologies as $tech){
+
+            $new_tech = new Technology();
+            $new_tech->tech_name = $tech;
+        }
+    }
+}
